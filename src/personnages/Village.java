025e21b1cc8a81c23b51ab89c215acebc703f7be
Lady1704 <celine.cholin@gmail.com>
2023-10-02 15:59:33 +0200

    TP1

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,32 +1,15 @@
 package personnages;
 
 public class Village {
-    private String nom;
-    private Chef chef;
-    private Gaulois[] villageois; 
-    private int nbVillageois;
-
-    public Village(String nom, int nbVillageoisMaximum) {
-        this.nom = nom;
-        this.villageois = new Gaulois[100]; 
-        this.nbVillageois = 0; 
-    }
-
-    public void setChef(Chef chef) {
-        this.chef = chef;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void ajouterVillageois(Gaulois gaulois) {
-        if (nbVillageois < villageois.length) {
-            villageois[nbVillageois] = gaulois;
-            nbVillageois++;
-        } else {
-            System.out.println("Le village est plein, pas de place pour plus de villageois.");
-        }
-    }
-
+	private String nom;
+	private Chef chef;
+	public Village(String nom) {
+		this.nom = nom;
+	}
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	public String getNom() {
+		return nom;
+	}
 }
